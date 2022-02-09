97ff7f61bba955a24ae7a6be4286314bd9dc6d10
Prasanna RV <prasannavelmurugan0200@gmail.com>
2022-02-09 10:59:29 +0530

    changes'

---
+++ routes/template.js
@@ -0,0 +1,25 @@
+if (process.env.NODE_ENV !== 'production') require('dotenv').config();
+const mysql = require('mysql');
+const express = require('express');
+const path = require('path');
+const router = express.Router({ mergeParams: true });
+const methodOverride = require('method-override');
+const multer = require('multer');
+const { storage, cloudinary } = require('../cloudinary');
+const upload = multer({ storage });
+const db = require('../database');
+const { isloggedin } = require('../middleware');
+
+
+
+router.get("/template1", (req, res, next) => {
+    res.render("./templates/template1", {
+        title: "Template 1",
+        user: req.user,
+    }
+    );
+});
+
+
+
+module.exports = router;
