af735b366497692ce7c7d572c3977d24b35ed038
Lohith <srklohith05@gmail.com>
2022-02-10 22:43:40 +0530

    Chnages

--- server.js
+++ server.js
@@ -2,7 +2,6 @@ if (process.env.NODE_ENV !== 'production') require('dotenv').config();
 const express = require('express');
 const ejsMate = require('ejs-mate');
 
-
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const cookieParser = require('cookie-parser');
@@ -12,13 +11,10 @@ const mysql = require('mysql');
 const db = require('./database');
 const compression = require('compression');
 
-
 const multer = require('multer');
 const morgan = require('morgan');
 const { storage, cloudinary } = require('./cloudinary');
 const upload = multer({ storage });
-
-
 
 const router = require('./routes/home');
 const auth = require('./routes/auth');
@@ -27,10 +23,7 @@ const templates = require('./routes/template');
 const contact = require('./routes/contact');
 const watermark = require('./routes/watermark');
 
-
-
 const app = express();
-
 
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
@@ -43,33 +36,34 @@ app.use(compression());
 
 app.use(cookieParser());
 
-app.use(session({
-    secret: "SecretSession",
-    cookie: { maxAge: 60000 },
-    resave: true,
-    saveUninitialized: true,
-}))
+app.use(
+	session({
+		secret: 'SecretSession',
+		cookie: { maxAge: 60000 },
+		resave: true,
+		saveUninitialized: true
+	})
+);
 
 app.use(flash());
 app.use((req, res, next) => {
-    res.locals.user = req.user;
-    res.locals.success_msg = req.flash('success_msg');
-    res.locals.error_msg = req.flash('error_msg');
-    next();
-})
+	res.locals.user = req.user;
+	res.locals.success_msg = req.flash('success_msg');
+	res.locals.error_msg = req.flash('error_msg');
+	next();
+});
 
+app.use('/', router);
+app.use('/en/templates', templates);
+app.use('/en/valentine', valentine);
+app.use('/en/', contact);
+app.use('/en/watermark', watermark);
+app.use('/auth', auth);
 
-
-app.use("/", router)
-app.use("/en/templates", templates)
-app.use("/en/valentine", valentine)
-app.use("/en/", contact)
-app.use("/en/watermark", watermark)
-app.use("/auth", auth)
-
-
-
+app.use('*', (req, res) => {
+	res.render('404');
+});
 
 app.listen(process.env.port, () => {
-    console.log(`Server is running in ${process.env.port}`);
-})
+	console.log(`Server is running in ${process.env.port}`);
+});
