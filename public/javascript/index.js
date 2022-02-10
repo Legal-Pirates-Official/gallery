ce57c32ee1b753e11c47f0c409eaa96c809be07b
Aravindhabii <abiiaravindhr.it2020@citchennai.net>
2022-02-10 22:24:54 +0530

    10.02.2022 Aravindh changes10:24pm

--- public/javascript/index.js
+++ public/javascript/index.js
@@ -1,23 +1,8 @@
-const navbar = document.querySelector(".navbar");
-const navbarright = document.querySelector(".navright");
-
-document.querySelector(".header").addEventListener("click", function () {
-  navbar.classList.add("active");
-  navbarright.classList.add("active2");
-  document.querySelector(".navright ul").style.display = "flex";
-  document.querySelector(".navright ul").style.transition = "all 2s";
-});
-document.querySelector(".closebtn").addEventListener("click", function () {
-  navbar.classList.remove("active");
-  navbarright.classList.remove("active2");
-  document.querySelector(".navright ul").style.display = "none";
-  document.querySelector(".navright ul").style.transition = "all 2s";
-});
-
 var swiper = new Swiper(".mySwiper", {
   loop: true,
   centeredSlides: true,
   slidesPerView: "3",
+  spaceBetween: 30,
   coverflowEffect: {
     rotate: 50,
     stretch: 0,
