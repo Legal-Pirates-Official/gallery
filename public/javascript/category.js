9c4e0d2647db13732ee192f35caca9f0be6fd10b
solomonsanthosh <solosanty2064@gmail.com>
2022-02-11 23:46:35 +0530

    SS

--- public/javascript/category.js
+++ public/javascript/category.js
@@ -1,1 +1,22 @@
-const fetch = fetch('http://localhost:8080/category/tomyvalentine').then(response => console.log(response)).catch(error => console.error(error));
+//
+const btn = document.querySelectorAll(".sectionbtn");
+btn.forEach((b) =>
+  b.addEventListener("click", async function  check(e)  {
+    // console.log(e);
+    const value = this;
+    const mode = value.getAttribute("data-name");
+    console.log(mode);
+    const fetch1 = await fetch(`http://localhost:8080/en/valentine/category/${mode}`,{
+
+        method: "GET",
+        redirect: 'follow',
+        headers: {
+            'Content-Type': 'application/json',
+          }
+
+    })
+      .then((response) => window.location = response.url)
+      .catch((error) => console.error(error));
+    // console.log(this.getAttribute('data-name'));
+  })
+);
